b3e87029b6ebadc69e3392143dae15c80df30ac3
Daniel Gerlinghoff <daniel@gerlinghoff.de>
2023-04-10 18:39:04 +0800

    Move math symbols around numpad Use Tap Dance to temporarily or permanently update NUM_NAV layer

--- qmk_files/keymap.c
+++ qmk_files/keymap.c
@@ -12,16 +12,40 @@ enum custom_keycodes {
     KC_SS,
 };
 
+
 /* TAP DANCE DEFINITIONS */
 enum {
     TD_QWERTY,
     TD_CAPS,
+    TD_NAVNUM,
 };
+
+void navnum_finished(qk_tap_dance_state_t *state, void *user_data);
+void navnum_reset(qk_tap_dance_state_t *state, void *user_data);
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_QWERTY] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_Q, _QWERTY),
     [TD_CAPS]   = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    [TD_NAVNUM] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, navnum_finished, navnum_reset),
 };
+
+typedef enum {
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+} td_state_t;
+
+static td_state_t td_state;
+
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        else                                       return TD_SINGLE_HOLD;
+    }
+    if (state->count == 2) return TD_DOUBLE_TAP;
+    else                   return TD_UNKNOWN;
+}
 
 
 /* KEYBOARD LAYERS */
@@ -45,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         // Home row
         KC_BSPC,
-        LT(_NAV_NUM, KC_A),
+        TD(TD_NAVNUM),
         LT(_SPEC, KC_R),
         LSFT_T(KC_S),
         LCTL_T(KC_T),
@@ -79,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         // Home row
         _______,
-        LT(_NAV_NUM, KC_A),
+        TD(TD_NAVNUM),
         LT(_SPEC, KC_S),
         LSFT_T(KC_D),
         LCTL_T(KC_F),
@@ -98,42 +122,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     *   ,-----------------------------------------------------------------------------------------------------.
     *   |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |          |
     *  ,-------+------+------+------+------+------+------+------+------+------+------+------+------+----------'
-    *  |       |   ß  |   _  |   [  |   ]  |   #  |   !  |   <  |   >  |   =  |   &  |   ä  |   ö  |    ü   |
+    *  |       |   ß  |   _  |   [  |   ]  |   !  |   /  |   <  |   >  |   =  |   -  |   ä  |   ö  |    ü   |
     * ,--------+------+------+------+------+-------------+------+------+------+------+------+------+--------'
-    * |        |   \  |   /  |   {  |   }  |   *  |   ?  |   (  |   )  |   -  |   :  |   @  |             |
+    * |        |   `  |   #  |   {  |   }  |   ?  |   *  |   (  |   )  |   &  |   +  |   @  |             |
     * |--------+------+------+------+------+------|------+------+------+------+------+------+--+-----+------.
-    * |        |   ^  |   ~  |   |  |   $  |   `  |   +  |   %  |   "  |   '  |   ;  |         |     |      |
+    * |        |   ^  |   ~  |   |  |   $  |   \  |   :  |   %  |   "  |   '  |   ;  |         |     |      |
     * `--------+------++-----+-+----+------+------+------+------+------+------+------++--------+-----+------|
     *  |       |       |       |                                              |       |        |     |      |
     *  `----------------------------------------------------------------------------------------------------'
     */
     [_SPEC] = LAYOUT(
-        _______, KC_F1,   KC_F2,      KC_F3,      KC_F4,      KC_F5,   KC_F6,      KC_F7,      KC_F8,      KC_F9,   KC_F10,     KC_F11,  KC_F12,  _______,
-        _______, KC_SS,   S(KC_MINS), KC_LBRC,    KC_RBRC,    S(KC_3), S(KC_1),    S(KC_COMM), S(KC_DOT),  KC_EQL,  S(KC_7),    KC_AE,   KC_OE,   KC_UE,
-        _______, KC_BSLS, KC_SLSH,    S(KC_LBRC), S(KC_RBRC), S(KC_8), S(KC_SLSH), S(KC_9),    S(KC_0),    KC_MINS, S(KC_SCLN), S(KC_2),      _______,
-        _______, S(KC_6), S(KC_GRV),  S(KC_BSLS), S(KC_4),    KC_GRV,  S(KC_EQL),  S(KC_5),    S(KC_QUOT), KC_QUOT, KC_SCLN,    _______, _______, _______,
-        _______, _______, _______,                            _______,                                              _______,    _______, _______, _______
+        _______, KC_F1,   KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,   KC_F10,    KC_F11,  KC_F12,  _______,
+        _______, KC_SS,   S(KC_MINS), KC_LBRC,    KC_RBRC,    S(KC_1),    KC_SLSH,    S(KC_COMM), S(KC_DOT),  KC_EQL,  KC_MINS,   KC_AE,   KC_OE,   KC_UE,
+        _______, KC_GRV,  S(KC_3),    S(KC_LBRC), S(KC_RBRC), S(KC_SLSH), S(KC_8),    S(KC_9),    S(KC_0),    S(KC_7), S(KC_EQL), S(KC_2),      _______,
+        _______, S(KC_6), S(KC_GRV),  S(KC_BSLS), S(KC_4),    KC_BSLS,    S(KC_SCLN), S(KC_5),    S(KC_QUOT), KC_QUOT, KC_SCLN,   _______, _______, _______,
+        _______, _______, _______,                            _______,                                                 _______,   _______, _______, _______
     ),
 
     /* Navigation and numpad layer
     *   ,-----------------------------------------------------------------------------------------------------.
     *   |      |      | DskLf|      | DskRt|      |      |   7  |   8  |   9  |      |      |      |          |
     *  ,-------+------+------+------+------+------+------+------+------+------+------+------+------+----------'
-    *  |       |      | Word | ArUp | Pste | Prev |      |   4  |   5  |   6  |      |      |      |        |
+    *  |       |      | Word | ArUp | Pste | Prev |   /  |   4  |   5  |   6  |   -  |      |      |        |
     * ,--------+------+------+------+------+-------------+------+------+------+------+------+------+--------'
-    * |        |      | ArLf | ArDn | ArRt |      |      |   1  |   2  |   3  |      |      |             |
+    * |        |      | ArLf | ArDn | ArRt |      |   *  |   1  |   2  |   3  |   +  |      |             |
     * |--------+------+------+------+------+------|------+------+------+------+------+------+--+-----+------.
-    * |        | Home | Cut  | Copy | End  | Pste |      |   0  |      |      |      |         |     |      |
+    * |        | Home | Cut  | Copy | End  | Pste |   :  |   0  |      |      |      |         |     |      |
     * `--------+------++-----+-+----+------+------+------+------+------+------+------++--------+-----+------|
     *  |       |       |       |                                              |       |        |     |      |
     *  `----------------------------------------------------------------------------------------------------'
     */
     [_NAV_NUM] = LAYOUT(
-        _______, _______, C(G(KC_LEFT)), _______, C(G(KC_RGHT)), _______,    _______, KC_7, KC_8,    KC_9,    _______, _______, _______, _______,
-        _______, _______, C(KC_RGHT),    KC_UP,   S(KC_INS),     C(KC_LEFT), _______, KC_4, KC_5,    KC_6,    _______, _______, _______, _______,
-        _______, _______, KC_LEFT,       KC_DOWN, KC_RGHT,       _______,    _______, KC_1, KC_2,    KC_3,    _______, _______,      _______,
-        _______, KC_HOME, C(KC_X),       C(KC_C), KC_END ,       C(KC_V),    _______, KC_0, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                               _______,                                     _______, _______, _______, _______
+        _______, _______, C(G(KC_LEFT)), _______, C(G(KC_RGHT)), _______,    _______,    KC_7, KC_8,    KC_9,    _______,   _______, _______, _______,
+        _______, _______, C(KC_RGHT),    KC_UP,   S(KC_INS),     C(KC_LEFT), KC_SLSH,    KC_4, KC_5,    KC_6,    KC_MINS,   _______, _______, _______,
+        _______, _______, KC_LEFT,       KC_DOWN, KC_RGHT,       _______,    S(KC_8),    KC_1, KC_2,    KC_3,    S(KC_EQL), _______,      _______,
+        _______, KC_HOME, C(KC_X),       C(KC_C), KC_END ,       C(KC_V),    S(KC_SCLN), KC_0, _______, _______, _______,   _______, _______, _______,
+        _______, _______, _______,                               _______,                                        _______,   _______, _______, _______
     ),
 };
 
@@ -149,6 +173,7 @@ void send_umlaut(char *lower, char *upper) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+        // Enable delete key on backspace
         case KC_BSPC: {
             static bool delkey_registered;
             if (record->event.pressed) {
@@ -170,9 +195,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             // Let QMK process the KC_BSPC keycode as usual outside of shift
-            return true;
+            break;
         }
 
+        // Write umlaute
         case KC_AE:
             if (record->event.pressed) send_umlaut("a", "A");
             break;
@@ -191,5 +217,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
     }
+
     return true;
 };
+
+void navnum_finished(qk_tap_dance_state_t *state, void *user_data) {
+    static bool navnum_layer_locked = false;
+
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            register_code(KC_A);
+            break;
+        case TD_SINGLE_HOLD:
+            layer_on(_NAV_NUM);
+            break;
+        case TD_DOUBLE_TAP:
+            if (!navnum_layer_locked) {
+                layer_on(_NAV_NUM);
+                navnum_layer_locked = true;
+            } else {
+                layer_off(_NAV_NUM);
+                navnum_layer_locked = false;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+void navnum_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            unregister_code(KC_A);
+            break;
+        case TD_SINGLE_HOLD:
+            layer_off(_NAV_NUM);
+            break;
+        default:
+            break;
+    }
+}
--- qmk_files/rules.mk
+++ qmk_files/rules.mk
@@ -14,3 +14,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 TAP_DANCE_ENABLE = yes      # Enable multi-tap functionality
 COMBO_ENABLE = no           # Enable combo feature
+SEND_STRING_ENABLE = yes    # Enable sending of strings
