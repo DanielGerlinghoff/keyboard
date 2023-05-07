8cd7de596b35899cb93db3b274a92355f18d9108
Daniel Gerlinghoff <daniel@gerlinghoff.de>
2023-05-07 14:51:23 +0800

    Specify files to be deployed individually

--- heatmap/deploy.sh
+++ heatmap/deploy.sh
@@ -8,11 +8,21 @@ exclude_list="deploy_credentials.sh,README.md"
 # Convert the list to an array
 IFS=',' read -r -a exclude_array <<< "$exclude_list"
 
+# Parse the command line arguments
+while getopts ":f:" opt; do
+  case $opt in
+    f) file_to_copy="$OPTARG";;
+    \?) echo "Invalid option: -$OPTARG" >&2;;
+  esac
+done
+
 # Loop through all the files in the current directory
 for file in *; do
   # Skip the script itself and the files in the exclude list
   if [[ "$file" != "$script_name" && ! " ${exclude_array[@]} " =~ " $file " ]]; then
     # Copy the file to the destination using scp
-    scp "$file" "$destination"
+    if [[ -z $file_to_copy ]] || [[ "$file" == "$file_to_copy" ]]; then
+      scp "$file" "$destination"
+    fi
   fi
 done
